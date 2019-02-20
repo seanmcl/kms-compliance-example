634803898f5a33bec7f9ba20a543561a29beff9f
Sean McLaughlin <seanmcl@gmail.com>
2019-02-20 17:18:08 -0500

    working

---
+++ src/main/java/Kms.java
@@ -0,0 +1,23 @@
+import com.amazonaws.services.kms.AWSKMS;
+import com.amazonaws.services.kms.AWSKMSClientBuilder;
+import com.amazonaws.services.kms.model.DataKeySpec;
+import com.amazonaws.services.kms.model.GenerateDataKeyRequest;
+import com.amazonaws.services.kms.model.GenerateDataKeyResult;
+import javax.crypto.spec.SecretKeySpec;
+
+class Kms {
+    static SecretKeySpec getKeySpec() {
+        AWSKMS kmsClient = AWSKMSClientBuilder.defaultClient();
+        final GenerateDataKeyRequest request = new GenerateDataKeyRequest()
+                .withKeySpec(DataKeySpec.AES_128)
+                .withKeyId("foo");
+
+        final GenerateDataKeyResult response = kmsClient.generateDataKey(request);
+        return new SecretKeySpec(response.getPlaintext().array(), "AES");
+    }
+
+    static int main(String[] args) {
+        getKeySpec();
+        return 0;
+    }
+}
