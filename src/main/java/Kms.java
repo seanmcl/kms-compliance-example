a85160ec44c770412fd7c543d0b2422c5991f054
Sean McLaughlin <seanmcl@gmail.com>
2019-02-24 09:51:16 -0500

    attempt to use maven

--- src/main/java/Kms.java
+++ src/main/java/Kms.java
@@ -1,16 +1,16 @@
 import com.amazonaws.services.kms.AWSKMS;
 import com.amazonaws.services.kms.AWSKMSClientBuilder;
-import com.amazonaws.services.kms.model.DataKeySpec;
 import com.amazonaws.services.kms.model.GenerateDataKeyRequest;
 import com.amazonaws.services.kms.model.GenerateDataKeyResult;
 import javax.crypto.spec.SecretKeySpec;
 
 class Kms {
+
     static SecretKeySpec getKeySpec() {
         AWSKMS kmsClient = AWSKMSClientBuilder.defaultClient();
+        String keySpec = "AES" + "_" + "256";
         final GenerateDataKeyRequest request = new GenerateDataKeyRequest()
-                .withKeySpec("AES_128");
-
+                .withKeySpec(keySpec);
         final GenerateDataKeyResult response = kmsClient.generateDataKey(request);
         return new SecretKeySpec(response.getPlaintext().array(), "AES");
     }
